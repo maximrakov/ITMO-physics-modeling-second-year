c9071757a7d184387f689b93fbfb87bb1f256984
maximrakov <78982680+maximrakov@users.noreply.github.com>
2022-05-24 11:57:00 +0300

    feat: add pyplot

--- model2/main.py
+++ model2/main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 S = 5
 alpha = 0.7
 currentTemperature = 37
@@ -5,6 +7,8 @@ eps = 2
 cBoar = 3100
 mBoar = 70
 
+time = []
+temp = []
 
 def get_d_temperature(current_temperature, dt):
     return (alpha * S * (current_temperature - environmentTemperature) * dt) / (cBoar * mBoar)
@@ -23,8 +27,18 @@ smT = 0
 while smT < waitingTime:
     currentTemperature -= get_d_temperature(currentTemperature, dt)
     smT += dt
+    time.append(smT)
+    temp.append(currentTemperature)
 
 if abs(currentTemperature - endTemperature) < eps:
     print("Yes")
 else:
     print("No")
+
+plt.plot(time, temp)
+plt.xlabel("Time")
+plt.ylabel("Temperature")
+plt.show()
+# 21
+# 29
+# 30000
